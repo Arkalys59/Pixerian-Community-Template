8e0df6c8970e12cfa96e16f9c6247d8fef64d60d
OrionPix <84450789+OrionPix-Dev@users.noreply.github.com>
2022-09-17 16:59:18 +0200

    ajout shop - ammunation

---
+++ resources/[SCRIPT]/Pixerian_Shop/UI/RageConfig.lua
@@ -0,0 +1,17 @@
+
+
+RageConfig = {}
+
+RageConfig.Header = {
+    textureDict = "commonmenu",-- "commonmenu",
+    textureName = "interaction_bgd", --"interaction_bgd"
+    color = {r = 0, g = 200, b = 200, a = 255}, -- {r = 255, g = 255, b = 255, a = 255}
+    GLARE = true,
+}
+
+RageConfig.Title = {
+    x = 215,
+    y = 22,
+    Scale = 0.90
+}
+
